99def8accd51a80683774dfc01c31928576d5f4e
ChenPt <847136903@qq.com>
2017-07-21 22:44:07 +0800

    添加css的打包...

--- src/Mask/index.js
+++ src/Mask/index.js
@@ -1,4 +1,7 @@
 // index.js
+// import style from './mask.css';
+
 var Mask = {}; 
+// console.log(style.test);
 
 export { Mask }
--- src/core.js
+++ src/core.js
@@ -2,12 +2,13 @@
 import { Popout } from './Popout'; 
 import Nav from './Nav'; 
 import Component from './Component'; 
-
+import { Mask } from './Mask';
+import css from './Mask/mask.css';
 
 var test = new Component({
 	name: 'test', 
-	tpl: '<h1> hello, world </h1>', 
-	style: ' test h1 { color: red } ', 
+	tpl: `<h1 class="${css.test}"> hello, world </h1>`, 
+	style: '', 
 	onInit: () => { console.log('oninit') },
 	onDestroy: () => {console.log('delete')}
 }); 
