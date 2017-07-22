abdc5cf412128ef022c092dcb537376a469cbe56
eczn <1975845422@qq.com>
2017-07-23 01:52:07 +0800

    数据绑定

--- src/Component/index.js
+++ src/Component/index.js
@@ -15,16 +15,59 @@ ComponentProto.render = function(){
 		</style>
 	`; 
 
+	window.h = $component; 
 	this.tplParser($component, config); 
-
+	
 	this.root = $component; 
 
 	return $component; 
 }
 
+var replacer = function(match, p1, p2, p3, offset, string){
+	return this[p1.trim()]; 
+}
+
+function dataBinding(root, config){
+	// 筛选文本结点 
+	Array.from(root.childNodes).filter(e => e.nodeType === 3).forEach(text => {
+		var exp = /{{(.*?)}}/g
+		  , matches = text.wholeText.match(exp);
+		
+		// 未有数据绑定 则返回
+		if (!matches) return; 
+
+		//  否则 
+		var tpl = text.wholeText
+		//  数据渲染 
+		  , render = () => text.data = tpl.replace(exp, replacer.bind(config.data)); 
+		
+		//  首次渲染
+		render();
+		
+		//  根据 matches 里的匹配对象进行 setter getter 绑定 
+		matches.map(e => e.slice(2, -2).trim()).forEach(varName => {
+			var preVal = config.data[varName]; 
+			Object.defineProperty(config.data, varName, {
+				get(){
+					return preVal; 
+				}, 
+				set(newVal){
+					preVal = newVal; 
+					render(); 
+					return newVal; 
+				}
+			});
+		}); 
+	})
+}
+
 // 递归遍历树 
 ComponentProto.tplParser = function(root, config){
 	// 遍历子树 
+
+	// 数据绑定 
+	dataBinding(root, config);
+
 	Array.from(root.children).forEach(child => {
 		// child 的属性 attributes 
 		Array.from(child.attributes).map(attr => {
