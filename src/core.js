c02a25041fe22a2c4f39eb061c47ceedc6737166
eczn <1975845422@qq.com>
2017-07-23 01:53:00 +0800

    修改模版

--- src/core.js
+++ src/core.js
@@ -5,12 +5,15 @@ import { Component } from './Component';
 import { Mask } from './Mask';
 import css from './Mask/mask.css';
 
+import { Alert } from './Alert'; 
+
 var test = new Component({
 	name: 'nav-push-pop-test', 
 	tpl: `
-		<div>
-			<h1 class="${css.test}">
-				hello, world
+		<div class="${css.test}">
+			<header>Hello</header>
+			<h1>
+				{{ wow }} ({{ VERSION }})
 			</h1>
 			<button @click="toPush"> Push ! </button>
 			<button @click="toPop"> Pop ! </button>
@@ -24,10 +27,15 @@ var test = new Component({
 		console.log('delete')
 	},
 	toPush(e){
+		console.log('toPush'); 
 		Nav.push(test); 
 	}, 
 	toPop(e){
 		Nav.pop(); 
+	}, 
+	data: {
+		wow: '数据绑定',
+		VERSION: '0.0.1'
 	}
 }); 
 
@@ -37,3 +45,11 @@ window.test = test;
 window.Nav = Nav; 
 window.Component = Component; 
 
+var N = {
+	Alert, 
+	Component
+}
+
+window.N = N; 
+
+export default N; 
